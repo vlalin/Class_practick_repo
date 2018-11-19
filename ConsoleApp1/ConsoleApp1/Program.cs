957ba2dccfbcd8d82892b7eeef5776af20cdaaa1
student <student@101-12>
2018-11-19 11:55:17 +0200

    optimized adding people

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,29 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            vlalin vlalin = new vlalin("vlad", "Linskiy", "Dmitievish", 21);
-            vlalin vova = new vlalin("Vova", "Lavrov", "Goose", 21);
-            vlalin ruslan = new vlalin("Ruslan", "Fateev", "Viktoovich", 21);
-            vlalin misha = new vlalin("Misha", "Kolygin", "Kolygin", 21);
             List<vlalin> Municipality = new List<vlalin>();
 
-            Municipality.Add(vlalin);
-           
-            Municipality.Add(vova);
-            
-            Municipality.Add(ruslan);
-            
-            Municipality.Add(misha);
-
+            Municipality.Add(new vlalin("vlad", "Linskiy", "Dmitievish", 21));
+            Municipality.Add(new vlalin("Vova", "Lavrov", "Goose", 21));
+            Municipality.Add(new vlalin("Ruslan", "Fateev", "Viktoovich", 21));
+            Municipality.Add(new vlalin("Misha", "Kolygin", "Kolygin", 21));
 
             Municipality.ForEach( x => x.info());
 
-
-
-
             Console.Read();
-
-
         }
     }
 }
